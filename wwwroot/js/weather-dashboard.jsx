5ede4d375aa9f709bdd33d481948502eb59cd16f
Holdmedve <talos.boti@gmail.com>
2024-01-02 21:11:02 +0100

    🚧 Get clicked city suggestion text

--- wwwroot/js/weather-dashboard.jsx
+++ wwwroot/js/weather-dashboard.jsx
@@ -4,9 +4,7 @@ const WeatherDashboard = (props) => {
     const [data, setData] = React.useState(props.initialData);
 
     queryCities = query => {
-        console.log(query);
         if (query === ""){
-            console.log("blablabla");
             return;
         }
 
@@ -14,13 +12,16 @@ const WeatherDashboard = (props) => {
             query: query
         }))
             .then(response => response.json())
-            .then(respData => {
-                setData(respData.cities);    
-            })
+            .then(respJson => {
+                console.log('search result');
+                console.log(respJson.cities);
+                setData(respJson.cities);    
+            });
     }
 
     handleInputChange = event => {
         const input = event.currentTarget.value;
+        console.log(`search input changed to: ${ input }`);
         clearTimeout(timer);
         setTimer(
             setTimeout(() => {
@@ -30,32 +31,55 @@ const WeatherDashboard = (props) => {
         );  
     }
 
+    handleOptionSelect = event => {
+        console.log("asd");
+        console.log('event:');
+        const myEvent = event;
+        console.log(myEvent);
+        console.log(myEvent.value);
+        console.log(myEvent.target);
+        console.log(myEvent.target.text);
+    }
+
+
     return (
         <div className="cityComponent">
-            <CitySearch listId="city-list" onInputChange={handleInputChange} />
-            <CityList id="city-list" data={data} />
+            <CitySearch 
+                onInputChange={handleInputChange} 
+            />
+            <CitySuggestionList 
+                id="city-list" 
+                data={data} 
+                onOptionSelect={handleOptionSelect}
+            />
         </div>
     );
 }
 
 const CitySearch = (props) => {
-    return <input list={props.listId} type="search" id="citySearch" onChange={props.onInputChange}/>
+    return <input 
+        type="text" 
+        id="citySearch"
+        // list="city-list" 
+        onChange={props.onInputChange}
+    />
 }
 
-const CityList = (props) => {
+const CitySuggestionList = (props) => {
     const cityNodes = props.data.map(function(city) {
+        console.log(`id is: ${city.id}`);
+        const val = `${city.name}, ${city.country}`;
         return (
-            <City country={city.country} key={city.id} city={city.name}>
-                {city.name}
-            </City>
-        );
+            <CitySuggestion key={city.id} value={val} onClick={props.onOptionSelect}></CitySuggestion>
+        )
+        
     });
-    return <datalist id={props.id}>{cityNodes}</datalist>;
+    // return <datalist key={props.id}>{cityNodes}</datalist>;
+    return <ul id="myUl">{ cityNodes }</ul>
 }
 
-const City = (props) => {
-    const val = `${props.city}, ${props.country}`;
+const CitySuggestion = (props) => {
     return (
-        <option value={val}>{val}</option>
+        <li onClick={props.onClick}><a href={`#${props.value}`} >{props.value}</a></li>
     );
 }

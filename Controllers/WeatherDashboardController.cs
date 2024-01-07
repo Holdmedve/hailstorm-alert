2dfa6dd128192d96b9d5cfeb04f0a79b69ff01a5
Holdmedve <talos.boti@gmail.com>
2024-01-07 20:24:39 +0100

    Create endpoint that maps requested alert info to c# model

--- Controllers/WeatherDashboardController.cs
+++ Controllers/WeatherDashboardController.cs
@@ -38,6 +38,65 @@ namespace ReactDemo.Controllers
             return bag;
         }
 
+        private static async Task<BagModel> getCityWeather(string cityId)
+        {
+            string apiKey = getWeatherApiKey();
+            if (apiKey == null || cityId == null || cityId == "")
+            {
+                return new BagModel {
+                    Cities = new List<CityModel>(),
+                    CityWeather = new CityWeather()
+                };
+            }
+
+            string searchUri =$"http://api.weatherapi.com/v1/forecast.json?key={apiKey}&q=id:{cityId}&days=3&aqi=no&alerts=yes";
+            HttpClient client = new HttpClient();
+            using HttpResponseMessage response = await client.GetAsync(searchUri);
+            string content = await response.Content.ReadAsStringAsync();
+
+            CityWeather cityWeather;
+            try {
+                cityWeather = JsonSerializer.Deserialize<CityWeather>(
+                    content, 
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    }                
+                );
+            }
+            catch (JsonException e){
+                Console.WriteLine(e.ToString());
+                return new BagModel {
+                    Cities = new List<CityModel>(),
+                    CityWeather = new CityWeather()
+                };
+
+            }
+            catch(ArgumentNullException e) {
+                Console.WriteLine(e.ToString());
+                return new BagModel {
+                    Cities = new List<CityModel>(),
+                    CityWeather = new CityWeather()
+                };
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                return new BagModel {
+                    Cities = new List<CityModel>(),
+                    CityWeather = new CityWeather()
+                };
+            }
+
+            Console.WriteLine(cityWeather.ToString());
+
+            BagModel bag = new BagModel{
+                Cities = new List<CityModel>(),
+                CityWeather = cityWeather    
+            };
+            return bag;
+        }
+
         [Route("weather-dashboard")]
         [HttpGet]
         public  async Task<ActionResult> Index()
@@ -52,9 +111,22 @@ namespace ReactDemo.Controllers
             return Json(await searchCity(query));
         }
 
+        [Route("city-weather")]
+        [HttpGet]
+        public async Task<ActionResult> CityWeather(string cityId)
+        {
+            Console.WriteLine($"cityId: {cityId}");
+            return Json(await getCityWeather(cityId));
+        }
+
         private static string getWeatherApiKey()
         {
-            return Environment.GetEnvironmentVariable("WEATHER_API_KEY");
+            string apiKey = Environment.GetEnvironmentVariable("WEATHER_API_KEY");
+            if (apiKey == null)
+            {
+                Console.WriteLine("WEATHER_API_KEY not set");
+            }
+            return apiKey;
         }
     }
 

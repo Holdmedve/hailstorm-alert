367898d725779a70c61c1cdf0bb87be65226cb32
Holdmedve <talos.boti@gmail.com>
2024-01-15 20:21:58 +0100

    Searchbar with suggestions, CORS localhost

---
+++ client/src/WeatherDashboard.js
@@ -0,0 +1,81 @@
+import { useState } from "react";
+import './WeatherDashboard.css';
+
+
+function WeatherDashboard() {
+    const [timer, setTimer] = useState(null);
+    const [searchQuery, setSearchQuery] = useState("");
+    const [suggestions, setSuggestions] = useState([]);
+
+    const url = "http://localhost:5146";
+
+    const queryCities = query => {
+        if (query === "") {
+            setSuggestions([]);
+            return;
+        }
+
+        console.log(`queryCities called: ${ query }`);
+        fetch(url + '/city-search/' + query)
+            .then(response => response.json())
+            .then(data => {
+                console.log('search result');
+                console.log(data);
+                setSuggestions(data);
+            });
+    }
+
+    const cityWeather = (cityId) => {
+        console.log(`cityWeather called with cityId: ${ cityId }`);
+        const numDays = 3;
+        fetch(url + '/city-weather/' + cityId + '/' + numDays)
+            .then(response => response.json())
+            .then(data => {
+                console.log('city weather result');
+                console.log(data);
+                // setSuggestions(data)
+            });
+    }
+
+    const onChange = (e) => {
+        const value = e.target.value;
+        console.log(`onChange called: ${ value }`);
+        clearTimeout(timer);
+        setTimer(
+            setTimeout(() => {
+                queryCities(value);
+                }, 250
+            )
+        ); 
+    }
+
+    const onSuggestionClicked = (cityId) => {
+        console.log(`onSuggestionClicked cityId: ${ cityId }`);
+        cityWeather(cityId);
+    }
+
+    return (
+        <div className="WeatherDashboard">
+            <input
+                type="text" 
+                className="search-bar"
+                id="city-search-bar"
+                placeholder="City..."
+                onChange={onChange}
+            />
+            <div>
+                {suggestions.map((suggestion) => {
+                    const cityId = suggestion.id;
+                    return (
+                        <div onClick={() => onSuggestionClicked(cityId)}>
+                            {suggestion.name}, {suggestion.country}
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+        
+    );
+}
+
+export default WeatherDashboard;
--- service/Program.cs
+++ service/Program.cs
@@ -5,7 +5,16 @@ namespace WeatherAlert;
 class Program {
     public static void Main(string[] args) {
         var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddCors( options => {
+            options.AddDefaultPolicy(builder => {
+                builder.SetIsOriginAllowed(origin => {
+                    return new Uri(origin).Host == "localhost";
+                });
+            });
+        });
+
         var app = builder.Build();
+        app.UseCors();
 
         app.MapGet("/", () => "Hello World!");
         app.MapGet("/city-search/{query}", Handlers.CitySearch);
